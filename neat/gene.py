fcf67d0d0bdfa6d5e43427a1c77351979a56e00f
dionislez <leznevskiy@mail.ru>
2024-06-05 23:28:20 +0700

    feat: refactor, annotations, comments

--- neat/gene.py
+++ neat/gene.py
@@ -1,14 +1,21 @@
 class Gene:
     """Класс генов."""
 
-    def __init__(self, in_node, out_node, weight, enabled, innovation):
-        self.in_node: int = in_node  # входной узел
-        self.out_node: int = out_node  # выходной узел
-        self.weight: int | float = weight  # вес соединения
-        self.enabled: bool = enabled  # есть ли соединение между узлами
+    def __init__(
+        self,
+        in_node: int,
+        out_node: int,
+        weight: float | int,
+        enabled: bool,
+        innovation: int,
+    ) -> None:
+        self.in_node: int = in_node  # входной нейрон
+        self.out_node: int = out_node  # выходной нейрон
+        self.weight: float | int = weight  # вес соединения
+        self.enabled: bool = enabled  # есть ли соединение между нейронами
         self.innovation: int = innovation  # инновационный номер для поиска соответствующих генов при скрещивании
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (
             f"Gene(in:{self.in_node}, "
             f"out:{self.out_node}, "
